0cf082554e57b252be3bcabea8736c43b77f69b6
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-03-29 20:37:26 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,21 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          
         </a>
+        <p>There are several universal free EPG (Electronic Program Guide) sources available on GitHub that you can use to retrieve TV guide information for various countries and channels. Here are some of the popular ones:
+
+1. WebGrab+Plus: It is a multi-site EPG and web scraping software that can fetch TV listings from more than 100 TV guide sources around the world. You can find the source code of WebGrab+Plus on GitHub at https://github.com/SilentButeo2/webgrabplus.
+
+2. XMLTV: It is another popular open-source project for retrieving EPG data from various sources. You can find the source code of XMLTV on GitHub at https://github.com/XMLTV/xmltv.
+
+3. TVHeadend: It is a Linux-based TV streaming server that also provides EPG data for different countries and channels. You can find the source code of TVHeadend on GitHub at https://github.com/tvheadend/tvheadend.
+
+4. EPG Collector: It is a Windows-based EPG grabber that can retrieve TV listings from various sources, including DVB and XMLTV. You can find the source code of EPG Collector on GitHub at https://github.com/IT2005/epgcollector.
+
+5. Schedules Direct: It is a subscription-based EPG service that offers TV guide data for the USA, Canada, and Mexico. You can find the source code of Schedules Direct on GitHub at https://github.com/SchedulesDirect/schedulesdirect-api.
+
+These are just a few examples of the many EPG sources available on GitHub. Depending on your requirements and location, you can choose the one that suits you the best.</p>
       </header>
     </div>
   );
