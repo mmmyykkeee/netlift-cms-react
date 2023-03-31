074b809868f1b8a6f2f9cab5d68b7edd6e91de71
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-03-31 18:00:28 +0300

    added markdown converter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Component/Header';
 
 function App() {
+  
   return (
     <div className='App'>
       <Header />
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,13 +1,31 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
 import RandomNavbar from './RandomNavbar'
 import Home from './Home'
+import Markdown from 'markdown-to-jsx';
 
 function Header() {
+
+  const file_name = 'react_pinterest_clone.md';
+  const [post, setPost] = useState('');
+
+  useEffect(() => {
+      import({file_name})
+          .then(res => {
+              fetch(res.default)
+                  .then(res => res.text())
+                  .then(res => setPost(res))
+                  .catch(err => console.log(err));
+          })
+          .catch(err => console.log(err));
+  });
+
   return (
     <div>
       <h1 className='text-center'>This is my first App</h1>
       <RandomNavbar />
       <Home />
+      <Markdown>{post}</Markdown>
     </div>
   )
 }
