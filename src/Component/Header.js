e74957a2e0aea00a70b1952c9ac4ff8ff74cb5e4
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-03-31 19:05:01 +0300

    new yml

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,30 +2,26 @@
 import React, { useState, useEffect } from 'react';
 import RandomNavbar from './RandomNavbar'
 import Home from './Home'
-import Markdown from 'markdown-to-jsx';
+import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+
 
 function Header() {
-
-  const file_name = '../components/mike.md';
-  const [post, setPost] = useState('');
+  const [content, setContent] = useState('');
 
   useEffect(() => {
-      import({file_name})
-          .then(res => {
-              fetch(res.default)
-                  .then(res => res.text())
-                  .then(res => setPost(res))
-                  .catch(err => console.log(err));
-          })
-          .catch(err => console.log(err));
-  });
+    axios.get('./README.md').then((response) => {
+      setContent(response.data);
+    });
+  }, []);
+  
 
   return (
     <div>
       <h1 className='text-center'>This is my first App</h1>
       <RandomNavbar />
       <Home />
-      <Markdown>{post}</Markdown>
+      <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   )
 }
